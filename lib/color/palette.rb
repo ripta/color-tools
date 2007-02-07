2a371afb6ce87d194376e4fdee92ad558cbaad66
austin <austin@8d9166c5-6811-0410-8782-ea71c07d3dbd>
2007-02-07 02:28:41 +0000

    First pass at reorganizing the Ruby PDF Tools hierarchy.
    git-svn-id: http://stonecode.svnrepository.com/svn/ruby_pdf/color-tools/trunk@153 8d9166c5-6811-0410-8782-ea71c07d3dbd

---
+++ lib/color/palette.rb
@@ -0,0 +1,15 @@
+#--
+# Colour management with Ruby.
+#
+# Copyright 2005 Austin Ziegler
+#   http://rubyforge.org/ruby-pdf/
+#
+#   Licensed under a MIT-style licence.
+#
+# $Id$
+#++
+
+require 'color'
+
+module Color::Palette
+end
